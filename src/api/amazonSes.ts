cd40e45caadcfdf9d1ccc987d28d49d2e46452eb
Suryanshomar7240 <suryu7240@gmail.com>
2024-09-20 23:25:05 +0530

    amazon ses

---
+++ src/api/amazonSes.ts
@@ -0,0 +1,36 @@
+import aws from 'aws-sdk';
+
+export const sendEmailUsingSes = async (reqParams:any)=>{
+    if(!reqParams.mailId || !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(reqParams.mailId) ){
+      return;
+    }
+    const ses = new aws.SES({
+        region: process.env.AWS_REGION, accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    })
+    const params = {
+        Destination: {
+            ToAddresses: [reqParams.mailId],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: 'This is a test email sent from Node.js using Amazon SES!',
+                    Charset: 'UTF-8',
+                },
+            },
+            Subject: {
+                Data: 'Test Email',
+                Charset: 'UTF-8',
+            },
+        },
+        Source: process.env.EMAIL??'',
+    };
+
+    try {
+        const data = await ses.sendEmail(params).promise();
+        console.log('Email sent successfully:', data.MessageId);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import { mailHotel } from "./api/email";
+import { sendEmailUsingSes } from "./api/amazonSes";
 
 const router = express.Router();
 router.post("/mailHotel", (req, res) => {
   mailHotel(req.query);
   res.status(200).send({"message":'Email sent Successfully'});
 });
+
+router.get("/mail-using-amazon", (req, res) => {
+  sendEmailUsingSes(req.query);
+  res.status(200).send({"message":'Email sent Successfully'});
+});
+
 export default router;
 
