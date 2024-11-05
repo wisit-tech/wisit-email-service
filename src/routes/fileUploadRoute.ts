1a10cab89ac93453d1022cdc5d192b49db3eb342
vishuuwu <vishukashyap0901@gmail.com>
2024-11-06 02:53:12 +0530

    temp: solution: updated file link

--- src/routes/fileUploadRoute.ts
+++ src/routes/fileUploadRoute.ts
@@ -14,7 +14,10 @@ fileUploadRouter.post("/upload", (req: Request, res: Response): void => {
     }
 
     // Construct the URL for the uploaded file
-    const fileUrl = `${process.env.SERVICE_ENDPOINT}/public/uploads/${req.file.filename}`;
+    const fileUrl = `/public/uploads/${req.file.filename}`;
+
+    // temporary till we get SSH to update the env file with the SERVICE_ENDPOINT var
+    // const fileUrl = `${process.env.SERVICE_ENDPOINT}/public/uploads/${req.file.filename}`;
 
     // Send the response with the URL
     res
